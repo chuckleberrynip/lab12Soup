2ec0e3e9ac4460e9517fc8178574bded58110fc5
chuckleberrynip <61253901+chuckleberrynip@users.noreply.github.com>
2020-11-30 22:28:21 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import SoupForm from './components/SoupForm';
+import SoupList from './components/SoupList';
+
+function App() {
+  return (
+    <div className="App">
+      <SoupList />
+    </div>
+  );
+}
+
+export default App;
